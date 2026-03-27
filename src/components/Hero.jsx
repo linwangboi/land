d69c8f7a406e47bbab5d4b60bb27dd069defea8d
linwangboi <linwangboi@gmail.com>
2026-03-27 09:01:09 +0000

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,19 @@
+import { useEffect, useState } from "react";
+
 export default function Hero() {
-    return <div></div>
+  const [mousePosition, setMousePosition] = useState({x: 0, y: 0});
+  useEffect(() => {
+    function handleMouseMove(e) {
+        setMousePosition({x: e.clientX, y: e.clientY});
+    }
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => window.removeEventListener('mousemove', handleMouseMove);
+  }, [])
+  return (
+    <section className="relative min-h-screen flex items-center justify-center pt-16 sm:pt-20 px-4 sm:px-6 lg:px-8 overflow-hidden">
+        <div className="absolute inset-0 opacity-30">
+        
+        </div>
+    </section>
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,8 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+
 export default function Navbar() {
+  const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false);
   return (
     <nav className="fixed top-0 w-full z-50 transition-all duration-300 bg-slate-950/20 backdrop-blur-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -37,8 +41,45 @@ export default function Navbar() {
               Testimonials
             </a>
           </div>
+          <button
+            className="md:hidden p-2 items-center text-gray-300 hover:text-white transition-colors"
+            onClick={() => setMobileMenuIsOpen((prev) => !prev)}
+          >
+            {mobileMenuIsOpen ? (
+              <X className="size-5 sm:size-6" />
+            ) : (
+              <Menu className="size-5 sm:size-6" />
+            )}
+          </button>
         </div>
-      </div>
+      </div> 
+      {mobileMenuIsOpen && (
+        <div className="md:hidden bg-slate-900/95 backdrop-blur-lg border-t border-slate-800 animate-in slide-in-from-top duration-300">
+          <div className='px-4 py-4 sm:py-6 space-y-3 sm:space-y-4'>
+            <a
+              href="#features"
+              onClick={() => setMobileMenuIsOpen(false)}
+              className="block text-gray-300 hover:text-white text-sm lg:text-base transition-colors"
+            >
+              Features
+            </a>
+            <a
+              href="#pricing"
+              onClick={() => setMobileMenuIsOpen(false)}
+              className="block text-gray-300 hover:text-white text-sm lg:text-base transition-colors"
+            >
+              Pricing
+            </a>
+            <a
+              href="#testimonials"
+              onClick={() => setMobileMenuIsOpen(false)}
+              className="block text-gray-300 hover:text-white text-sm lg:text-base transition-colors"
+            >
+              Testimonials
+            </a>          
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
